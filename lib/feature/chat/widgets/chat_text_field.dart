c0d87e38cd2b6e8d33e51586310d55fb69081235
TheDP66 <dharma66putra@gmail.com>
2023-04-22 18:25:44 +0700

    Add - Modal attach file

--- lib/feature/chat/widgets/chat_text_field.dart
+++ lib/feature/chat/widgets/chat_text_field.dart
@@ -24,6 +24,7 @@ class _ChatTextFieldState extends ConsumerState<ChatTextField> {
   late TextEditingController messageController;
 
   bool isMessageIconEnabled = false;
+  double cardHeight = 0;
 
   void sendTextMessage() async {
     if (isMessageIconEnabled) {
@@ -55,6 +56,38 @@ class _ChatTextFieldState extends ConsumerState<ChatTextField> {
     );
   }
 
+  iconWithText({
+    required VoidCallback onPressed,
+    required IconData icon,
+    required String text,
+    required Color background,
+  }) {
+    return Column(
+      children: [
+        CustomIconButton(
+          onTap: onPressed,
+          icon: icon,
+          background: background,
+          minWidth: 50,
+          iconColor: Colors.white,
+          border: Border.all(
+            color: context.theme.greyColor!.withOpacity(.2),
+            width: 1,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            color: context.theme.greyColor,
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   void initState() {
     messageController = TextEditingController();
@@ -69,77 +102,162 @@ class _ChatTextFieldState extends ConsumerState<ChatTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextFormField(
-              controller: messageController,
-              maxLines: 4,
-              minLines: 1,
-              onChanged: (value) {
-                value.isEmpty
-                    ? setState(() => isMessageIconEnabled = false)
-                    : setState(() => isMessageIconEnabled = true);
-              },
-              decoration: InputDecoration(
-                hintText: "Message",
-                hintStyle: TextStyle(
-                  color: context.theme.greyColor,
-                ),
-                filled: true,
-                fillColor: context.theme.chatTextFieldBg,
-                isDense: true,
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    style: BorderStyle.none,
-                    width: 0,
-                  ),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                prefixIcon: Material(
-                  color: Colors.transparent,
-                  child: CustomIconButton(
-                    onTap: () {},
-                    icon: Icons.emoji_emotions_outlined,
-                    iconColor: Theme.of(context).listTileTheme.iconColor,
-                  ),
-                ),
-                suffixIcon: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    RotatedBox(
-                      quarterTurns: 45,
-                      child: CustomIconButton(
-                        onTap: () {},
-                        icon: Icons.attach_file,
-                        iconColor: Theme.of(context).listTileTheme.iconColor,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        AnimatedContainer(
+          duration: const Duration(
+            milliseconds: 200,
+          ),
+          height: cardHeight,
+          width: double.maxFinite,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 10,
+          ),
+          decoration: BoxDecoration(
+            color: context.theme.receiverChatCardBg,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      iconWithText(
+                        onPressed: () {},
+                        icon: Icons.book,
+                        text: "File",
+                        background: const Color(0xFF7F66FE),
                       ),
-                    ),
-                    CustomIconButton(
-                      onTap: () {},
-                      icon: Icons.camera_alt_outlined,
-                      iconColor: Theme.of(context).listTileTheme.iconColor,
-                    ),
-                  ],
-                ),
+                      iconWithText(
+                        onPressed: () {},
+                        icon: Icons.camera_alt,
+                        text: "Camera",
+                        background: const Color(0xFFFE2E74),
+                      ),
+                      iconWithText(
+                        onPressed: () {},
+                        icon: Icons.photo,
+                        text: "Gallery",
+                        background: const Color(0xFFC861F9),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      iconWithText(
+                        onPressed: () {},
+                        icon: Icons.headphones,
+                        text: "Audio",
+                        background: const Color(0xFFF96533),
+                      ),
+                      iconWithText(
+                        onPressed: () {},
+                        icon: Icons.location_on,
+                        text: "Location",
+                        background: const Color(0xFF1FA855),
+                      ),
+                      iconWithText(
+                        onPressed: () {},
+                        icon: Icons.person,
+                        text: "Contact",
+                        background: const Color(0xFF009DE1),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
-          const SizedBox(
-            width: 5,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: messageController,
+                  maxLines: 4,
+                  minLines: 1,
+                  onChanged: (value) {
+                    value.isEmpty
+                        ? setState(() => isMessageIconEnabled = false)
+                        : setState(() => isMessageIconEnabled = true);
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Message",
+                    hintStyle: TextStyle(
+                      color: context.theme.greyColor,
+                    ),
+                    filled: true,
+                    fillColor: context.theme.chatTextFieldBg,
+                    isDense: true,
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        style: BorderStyle.none,
+                        width: 0,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    prefixIcon: Material(
+                      color: Colors.transparent,
+                      child: CustomIconButton(
+                        onTap: () {},
+                        icon: Icons.emoji_emotions_outlined,
+                        iconColor: Theme.of(context).listTileTheme.iconColor,
+                      ),
+                    ),
+                    suffixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        RotatedBox(
+                          quarterTurns: 45,
+                          child: CustomIconButton(
+                            onTap: () => setState(
+                              () {
+                                cardHeight == 0
+                                    ? cardHeight = 220
+                                    : cardHeight = 0;
+                              },
+                            ),
+                            icon: cardHeight == 0
+                                ? Icons.attach_file
+                                : Icons.close,
+                            iconColor:
+                                Theme.of(context).listTileTheme.iconColor,
+                          ),
+                        ),
+                        CustomIconButton(
+                          onTap: () {},
+                          icon: Icons.camera_alt_outlined,
+                          iconColor: Theme.of(context).listTileTheme.iconColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              CustomIconButton(
+                onTap: sendTextMessage,
+                icon: isMessageIconEnabled
+                    ? Icons.send_outlined
+                    : Icons.mic_none_outlined,
+                background: Coloors.greenDark,
+                iconColor: Colors.white,
+              ),
+            ],
           ),
-          CustomIconButton(
-            onTap: sendTextMessage,
-            icon: isMessageIconEnabled
-                ? Icons.send_outlined
-                : Icons.mic_none_outlined,
-            background: Coloors.greenDark,
-            iconColor: Colors.white,
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
